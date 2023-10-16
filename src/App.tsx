e9cff9058ebe66bd5ea85414bde0b3254355884d
SoniSSk <kuldeepsoni3099@gmail.com>
2023-10-16 23:25:43 +0530

    :aaa

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import './App.css';
-import Header from './componet/Header/Header';
 import AboutUs from './componet/AboutUs/AboutUs';
 import Footer from './componet/Footer/Footer';
 import ContactUs from './componet/ContactUs/ContactUs';
@@ -7,6 +6,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './componet/Dashboard/Dashboard';
 import CSRActivity from './componet/CSRActivity/CSRActivity';
 import Services from './componet/Services/Services';
+import Certificates from './componet/Certificates/Certificates';
 
 function App() {
   return <>
@@ -15,7 +15,7 @@ function App() {
         <Route path="/gomti_infra_mining" element={<Dashboard />} />
         <Route path="/gomti_infra_mining/about" element={<AboutUs />} />
         <Route path="/gomti_infra_mining/services" element={<Services />} />
-        <Route path="/gomti_infra_mining/certificate" element={<Services />} />
+        <Route path="/gomti_infra_mining/certificate" element={<Certificates />} />
         <Route path="/gomti_infra_mining/csr-activity" element={<CSRActivity />} />
         <Route path="/gomti_infra_mining/contact" element={<ContactUs />} />
       </Routes>
